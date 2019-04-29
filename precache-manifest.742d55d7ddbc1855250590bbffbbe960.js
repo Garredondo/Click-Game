dfce5d0c531127eeb7065f9a609962815fa86efd
George Arredondo <arredondo.george@gmail.com>
2019-04-29 14:42:22 -0500

    Updates

---
+++ precache-manifest.742d55d7ddbc1855250590bbffbbe960.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b3738a537103fd58eeea16c2949bbe",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "8206ddf9d351031d494c",
+    "url": "/click-game/static/js/2.cf0d62a8.chunk.js"
+  },
+  {
+    "revision": "5f1425cc6fb692cc01dd",
+    "url": "/click-game/static/js/main.3533d22f.chunk.js"
+  },
+  {
+    "revision": "aa765b97c1e7aaa53c3e",
+    "url": "/click-game/static/js/runtime~main.7d043b49.js"
+  }
+]);
